02d12686b58bbe1b603547e61ab493fda83bc4d6
KitsuneMamori <42525541+KitsuneMamori@users.noreply.github.com>
2019-12-02 12:19:36 +0100

    Update terms-of-conditions.js

--- commands/rules/terms-of-conditions.js
+++ commands/rules/terms-of-conditions.js
@@ -6,6 +6,9 @@ module.exports = {
         try {
             message.delete();
             message.channel.send('https://cdn.discordapp.com/attachments/494612992093192212/651016013688864769/Terms_of_Conditions.pdf')
+            .then(message => {
+                message.delete(5000)
+            })
         } catch (e) {
             console.log(`ERROR: ${e.message}`)
         }
